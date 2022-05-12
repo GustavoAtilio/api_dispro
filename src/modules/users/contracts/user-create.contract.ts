87119253ba50cbf12e003a3dce8f487a1a785742
GustavoAtilio <atiliodalcanton@gmail.com>
2022-05-11 21:01:16 -0300

    ajustes nas msg de errors

--- src/modules/users/contracts/user-create.contract.ts
+++ src/modules/users/contracts/user-create.contract.ts
@@ -8,25 +8,25 @@ export class UserCreateContract implements Contract{
         const flunt = new Flunt()
 
         //Validação Nome de Usuário
-        flunt.hasMinLen(model.name, 5, 'Informe um nome de usuário correto.');
-        flunt.hasMaxLen(model.name, 100, 'Informe um nome de usuário correto.');
-        flunt.isNotNull(model.name, "Informe um nome de usuário correto.")
+        flunt.hasMinLen(model.name, 5, 'Nome deve ser maior que 5 caracteres.');
+        flunt.hasMaxLen(model.name, 100, 'deve ser menor que 100 caracteres.');
+        //flunt.isNotNull(model.name, "Informe um nome de usuário correto.")
 
         //Validação Senha do Usuário
-        flunt.hasMinLen(model.password, 8, 'Informe uma senha correta.');
-        flunt.hasMaxLen(model.password, 60, 'Informe uma senha correta.');
-        flunt.isNotNull(model.password, "Informe uma senha correta.")
+        flunt.hasMinLen(model.password, 8, 'Senha deve ser maior que 8 caracteres.');
+        flunt.hasMaxLen(model.password, 60, 'Senha deve der Menor que 60 caracteres');
+        //flunt.isNotNull(model.password, "Informe uma senha correta.")
 
         //Validação E-mail do Usuário
-        flunt.hasMinLen(model.email, 5, 'Informe um e-mail correto.');
-        flunt.hasMaxLen(model.email, 100, 'Informe um e-mail correto.');
-        flunt.isNotNull(model.email, "Informe um e-mail correto.")
-        flunt.isEmail(model.email, "Informe um e-mail correto.")
+        flunt.hasMinLen(model.email, 5, 'E-mail deve ser maior que 5 caracteres.');
+        flunt.hasMaxLen(model.email, 100, 'E-mail deve ser menor que 60 caracteres');
+        //flunt.isNotNull(model.email, "Informe um e-mail correto.")
+        //flunt.isEmail(model.email, "E-mail não pode ser nulo.")
 
         //Validação Numerro do Usuário
-        flunt.hasMinLen(model.number, 9, 'Informe um número correto.');
-        flunt.hasMaxLen(model.number, 11, 'Informe um número correto.');
-        flunt.isNotNull(model.number, "Informe um número correto.")
+        //flunt.hasMinLen(model.number, 9, 'Número deve ser maior que 9 caracteres');
+        //flunt.hasMaxLen(model.number, 11, 'Número deve ser menor que 11 caracteres');
+        //flunt.isNotNull(model.number, "Informe um número correto.")
 
         
 
--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -17,16 +17,25 @@ export class UserController{
     @UseInterceptors(new ValidatorInterceptor(new UserCreateContract()))
     async createUser(@Body() body:UserEntity){
         try{
-            const user = await this.createUser(body);
+            const user = await this.service.createUserService(body);
             return new ResponseDto('Usuário Criado com Sucesso.',true,user,null);
         }catch(e){
-            throw new HttpException(new ResponseDto('Ops',false, null,e,),HttpStatus.BAD_REQUEST)
+            
+            throw new HttpException(new ResponseDto('ops',false,null,e),HttpStatus.BAD_REQUEST)
         }
     }
 
     @Post('/login')
     @UseInterceptors(new ValidatorInterceptor(new UserLoginContract()))
     async loginUser(@Body() body:UserEntity){
+
+        try{
+            const user = await this.service.loginUserService(body);
+            return new ResponseDto('Usuário Autenticado..',true,user,null);
+        }catch(e){
+            
+            throw new HttpException(new ResponseDto('ops',false,null,e),HttpStatus.BAD_REQUEST)
+        }
 
     }
     
